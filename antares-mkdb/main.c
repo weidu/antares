91dddc2eb144ef54a88e985056646ddd04687db6
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-03-05 22:27:20 +0100

    mkdb: automatic output filename

--- antares-mkdb/main.c
+++ antares-mkdb/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <chip/db.h>
 #include <chip/store.h>
@@ -8,21 +9,40 @@
 #include "typelist.h"
 #include "chip.h"
 
+static char *mk_outname(char *inname)
+{
+	char *c;
+	int r;
+	char *out;
+	
+	c = strrchr(inname, '.');
+	if(c != NULL)
+		*c = 0;
+	r = asprintf(&out, "%s.acg", inname);
+	if(r == -1) abort();
+	return out;
+}
+
 int main(int argc, char *argv[])
 {
 	struct db *db;
+	char *inname;
+	char *outname;
 	
-	if(argc != 3) {
-		fprintf(stderr, "Usage: antares-mkdb <input.xdlrc> <output.acg>\n");
+	if(argc != 2) {
+		fprintf(stderr, "Usage: antares-mkdb <input.xdlrc>\n");
 		exit(EXIT_FAILURE);
 	}
 	
+	inname = argv[1];
 	printf("Listing types...\n");
-	db = typelist_create_db(argv[1]);
+	db = typelist_create_db(inname);
 	printf("Listing chip components...\n");
-	chip_update_db(db, argv[1]);
+	chip_update_db(db, inname);
 	printf("Writing output file...\n");
-	db_write_file(db, argv[2]);
+	outname = mk_outname(inname);
+	db_write_file(db, outname);
+	free(outname);
 	db_free(db);
 	
 	return 0;
