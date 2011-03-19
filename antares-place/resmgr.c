096366ee1adbfb42aebf42c5a018ece5e903f7a1
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-03-20 00:30:34 +0100

    place: populate BEL lists

--- antares-place/resmgr.c
+++ antares-place/resmgr.c
@@ -7,6 +7,7 @@
 #include <anetlist/bels.h>
 #include <chip/db.h>
 
+#include "rtree.h"
 #include "resmgr.h"
 
 static int cmp_control_set(struct resmgr_control_set *cs1, struct resmgr_control_set *cs2)
@@ -67,23 +68,145 @@ static void populate_control_sets(struct resmgr *r, struct anetlist *a)
 	}
 }
 
+enum {
+	RESMGR_SITE_SLICEX,
+	RESMGR_SITE_SLICEL,
+	RESMGR_SITE_SLICEM,
+	RESMGR_SITE_TIEOFF,
+	RESMGR_SITE_RAMB8BWER,
+	RESMGR_SITE_RAMB16BWER,
+	RESMGR_SITE_DSP48A1,
+	RESMGR_SITE_IOBM,
+	RESMGR_SITE_IOBS,
+	RESMGR_SITE_ILOGIC2,
+	RESMGR_SITE_OLOGIC2,
+	RESMGR_SITE_IODELAY2,
+	RESMGR_SITE_BUFH,
+	RESMGR_SITE_BUFGMUX,
+};
+
+static int get_site_index(const char *name)
+{
+	if(strcmp(name, "SLICEX") == 0) return RESMGR_SITE_SLICEX;
+	if(strcmp(name, "SLICEL") == 0) return RESMGR_SITE_SLICEL;
+	if(strcmp(name, "SLICEM") == 0) return RESMGR_SITE_SLICEM;
+	if(strcmp(name, "TIEOFF") == 0) return RESMGR_SITE_TIEOFF;
+	if(strcmp(name, "RAMB8BWER") == 0) return RESMGR_SITE_RAMB8BWER;
+	if(strcmp(name, "RAMB16BWER") == 0) return RESMGR_SITE_RAMB16BWER;
+	if(strcmp(name, "DSP48A1") == 0) return RESMGR_SITE_DSP48A1;
+	if(strcmp(name, "IOBM") == 0) return RESMGR_SITE_IOBM;
+	if(strcmp(name, "IOBS") == 0) return RESMGR_SITE_IOBS;
+	if(strcmp(name, "ILOGIC2") == 0) return RESMGR_SITE_ILOGIC2;
+	if(strcmp(name, "OLOGIC2") == 0) return RESMGR_SITE_OLOGIC2;
+	if(strcmp(name, "IODELAY2") == 0) return RESMGR_SITE_IODELAY2;
+	if(strcmp(name, "BUFH") == 0) return RESMGR_SITE_BUFH;
+	if(strcmp(name, "BUFGMUX") == 0) return RESMGR_SITE_BUFGMUX;
+	return -1;
+}
+
+static void add_bel(struct resmgr *r, struct rtree_node *root, int type, int tile, int site_offset, int bel_offset)
+{
+	struct resmgr_bel *bel;
+	struct tile *t;
+	
+	t = &r->db->chip.tiles[tile];
+	
+	bel = alloc_type(struct resmgr_bel);
+	bel->type = type;
+	bel->tile = tile;
+	bel->site_offset = site_offset;
+	bel->bel_offset = bel_offset;
+	bel->inst = t->user;
+	t->user = bel;
+	rtree_add(root, bel);
+}
+
+static void populate_resources(struct resmgr *r, struct db *db)
+{
+	int i, j, k;
+	struct tile *tile;
+	struct tile_type *tile_type;
+	char *site_name;
+	int site_index;
+	
+	for(i=0;i<db->chip.w*db->chip.h;i++) {
+		tile = &db->chip.tiles[i];
+		tile_type = &db->tile_types[tile->type];
+		for(j=0;j<tile_type->n_sites;j++) {
+			site_name = db->site_types[tile_type->sites[j]].name;
+			site_index = get_site_index(site_name);
+			switch(site_index) {
+				case RESMGR_SITE_SLICEM:
+					/* fall through */
+				case RESMGR_SITE_SLICEL:
+					add_bel(r, r->free_carry4, ANETLIST_BEL_CARRY4, i, j, 0);
+					/* fall through */
+				case RESMGR_SITE_SLICEX:
+					for(k=0;k<4;k++)
+						add_bel(r, r->free_lut, ANETLIST_BEL_LUT6_2, i, j, k);
+					for(k=0;k<8;k++)
+						add_bel(r, r->free_fd, ANETLIST_BEL_FDRE, i, j, k);
+					break;
+				case RESMGR_SITE_IOBM:
+					add_bel(r, r->free_iobm, ANETLIST_BEL_IOBM, i, j, 0);
+					break;
+				case RESMGR_SITE_IOBS:
+					add_bel(r, r->free_iobs, ANETLIST_BEL_IOBS, i, j, 0);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
+
 struct resmgr *resmgr_new(struct anetlist *a, struct db *db)
 {
 	struct resmgr *r;
+	int i;
 	
 	r = alloc_type(struct resmgr);
+	
+	r->a = a;
+	r->db = db;
 	
 	r->n_control_sets = 0;
 	r->control_sets = NULL;
 	populate_control_sets(r, a);
+	printf("Number of unique control sets:\t%d\n", r->n_control_sets);
 	
-	printf("Number of unique control sets: %d\n", r->n_control_sets);
+	r->free_lut = rtree_new_root();
+	r->free_fd = rtree_new_root();
+	r->free_fd_cs = alloc_size(r->n_control_sets*sizeof(struct rtree_node *));
+	for(i=0;i<r->n_control_sets;i++)
+		r->free_fd_cs[i] = rtree_new_root();
+	r->free_carry4 = rtree_new_root();
+	r->free_iobm = rtree_new_root();
+	r->free_iobs = rtree_new_root();
+	r->used_resources = rtree_new_root();
+	populate_resources(r, db);
+	printf("Available LUTs:\t\t\t%d\n", r->free_lut->count);
+	printf("Available FDs:\t\t\t%d\n", r->free_fd->count);
+	printf("Available CARRY4s:\t\t%d\n", r->free_carry4->count);
+	printf("Available IOBMs:\t\t%d\n", r->free_iobm->count);
+	printf("Available IOBSs:\t\t%d\n", r->free_iobs->count);
 	
 	return r;
 }
 
 void resmgr_free(struct resmgr *r)
 {
+	int i;
+	
+	rtree_free(r->free_lut, free);
+	rtree_free(r->free_fd, free);
+	for(i=0;i<r->n_control_sets;i++)
+		rtree_free(r->free_fd_cs[i], free);
+	free(r->free_fd_cs);
+	rtree_free(r->free_carry4, free);
+	rtree_free(r->free_iobm, free);
+	rtree_free(r->free_iobs, free);
+	rtree_free(r->used_resources, free);
 	free(r->control_sets);
 	free(r);
 }
--- antares-place/resmgr.h
+++ antares-place/resmgr.h
@@ -4,6 +4,8 @@
 #include <anetlist/net.h>
 #include <chip/db.h>
 
+#include "rtree.h"
+
 struct resmgr_net_driver {
 	struct anetlist_instance *inst;
 	int output;
@@ -15,9 +17,28 @@ struct resmgr_control_set {
 	struct resmgr_net_driver sr;
 };
 
+struct resmgr_bel {
+	struct rtree_leaf_header rlh;		/* < rtree housekeeping */
+	int type;				/* < index in anetlist_bels */
+	int tile;				/* < offset in the chip DB tile array */
+	int site_offset;			/* < site offset inside the tile */
+	int bel_offset;				/* < BEL offset inside the site */
+	struct anetlist_instance *inst;		/* < netlist instance cross-reference */
+	struct resmgr_bel *next;		/* < next BEL in the tile */
+};
+
 struct resmgr {
-	int n_control_sets;
-	struct resmgr_control_set *control_sets;
+	struct anetlist *a;				/* < associated netlist */
+	struct db *db;					/* < associated chip database */
+	int n_control_sets;				/* < number of unique control sets */
+	struct resmgr_control_set *control_sets;	/* < description of the control sets */
+	struct rtree_node *free_lut;			/* < available LUT6_2s */
+	struct rtree_node *free_fd;			/* < available FDs that are not bound to a control set */
+	struct rtree_node **free_fd_cs;			/* < for each control set, list of available FDs bound to it */
+	struct rtree_node *free_carry4;			/* < available CARRY4s */
+	struct rtree_node *free_iobm;			/* < available IOBMs */
+	struct rtree_node *free_iobs;			/* < available IOBSs */
+	struct rtree_node *used_resources;		/* < all used resources */
 };
 
 struct resmgr *resmgr_new(struct anetlist *a, struct db *db);
