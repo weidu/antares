7cf32b6cd049e853e3df97608b9a1e3560371df2
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-04-18 20:11:56 +0200

    Add site lister sample application

---
+++ test-sitelist/main.c
@@ -0,0 +1,46 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <chip/db.h>
+#include <chip/load.h>
+
+static void list_sites(struct db *db)
+{
+	int clexm, clelm;
+	int i;
+	struct tile *tile;
+	
+	clexm = db_resolve_tile(db, "CLEXM");
+	clelm = db_resolve_tile(db, "CLELM");
+	for(i=0;i<db->chip.w*db->chip.h;i++) {
+		tile = &db->chip.tiles[i];
+		if((tile->type == clexm) || (tile->type == clelm))
+			printf("%s_X%dY%d %s\n", db->tile_types[tile->type].name, tile->x, tile->y,
+				tile->sites[0].name);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	struct db *db;
+	char *dbfile;
+
+	if(argc != 2) {
+		fprintf(stderr, "Usage: test-sitelist <db.acg>\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	dbfile = argv[1];
+
+	printf("Reading chip database...\n");
+	db = db_load_file(dbfile);
+	printf("...done.\n\n");
+	
+	list_sites(db);
+	
+	db_free(db);
+	
+	return 0;
+}
